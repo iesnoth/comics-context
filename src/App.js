c93910ae85dfb55b77cc4ed8a618a31c94f66c7d
Victoria Beasley <iesnoth91@gmail.com>
2022-11-19 20:04:08 -0600

    my own mild mods

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import StylesContext from './StylesContext';
 
 function App() {
   const styling = {
-    background: "#1ECD97",
+    background: "crimson",
     display: "inline-block",
     width: 120,
     height: 55,
     fontSize: 18,
     letterSpacing: 1,
-    border: "2px solid #1ECD97",
-    borderRadius: 15
+    border: "2px solid crimson",
+    borderRadius: 15,
+    margin: 5
 
   }
   return (
@@ -23,7 +24,8 @@ function App() {
         style={{
           color: "#FEE001",
           fontFamily: "Luminari, fantasy",
-          fontSize: 50
+          fontSize: 50,
+          webkitTextStroke: "2px black",
         }}
       >
         Welcome to Comics Galore!
--- src/SubmitForm.js
+++ src/SubmitForm.js
@@ -2,18 +2,23 @@ import React from "react";
 import RequestFormButton from './RequestFormButton'
 
 export default function SubmitForm() {
+  const inputStyling={
+    webkitTextStroke: "1px lightgrey",
+    margin: 3,
+    fontWeight: 800
+    }
   return (
     <div style={{display: "flex"}}>
       <form style={{display: "inline-block"}}>
-        <label>
+        <label style={inputStyling}>
           Name of Comic:
           <input type="text" />
         </label>
-        <label>
+        <label style={inputStyling}>
           Year of Release:
           <input type="text" />
         </label>
-        <label>
+        <label style={inputStyling}>
           Your Email:
           <input type="text" />
         </label>
